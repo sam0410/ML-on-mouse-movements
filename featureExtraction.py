12eb549364fdce7f080d67e60fc5dad95d6bdc2b
Samikshya Chand <samikshya.chand.ece15@itbhu.ac.in>
2017-10-01 16:05:41 +0530

    Add a new feature number of clicks

--- featureExtraction.py
+++ featureExtraction.py
@@ -5,13 +5,14 @@ feature 3 (TimeSpent):calculates time duration user spends online
 feature 4 (avgMoves):calculates avg number of by mouse movements per second
 feature 5 (avgx):avg x coordinate of mouse pointer
 feature 6 (avgy):avg y coordinate of mouse pointer
+feature 7 (numberOfClicks):number of mouse clicks in the session
 '''
 FileList=["session_0041905381","session_1060325796","session_3320405034",  "session_3826583375"  ,"session_6668463071","session_8961330453","session_9017095287"]
 
 #FileList=["session_0041905381"]
 import numpy as np
 
-featureVector=[None] * 6
+featureVector=[None] * 7
 
 
 for fileName in FileList:
@@ -22,6 +23,8 @@ for fileName in FileList:
 	avgSpeed=0
 	meanx=0
 	meany=0
+	numberOfClicks=0
+	
         TimeSpent=data[rows-1,0].astype(np.float)-data[0,0].astype(np.float)
 
 	for i in range (1,rows):
@@ -31,10 +34,12 @@ for fileName in FileList:
 		
 		meanx=	meanx+(data[i-1,4].astype(np.float))*(data[i,0].astype(np.float)-data[i-1,0].astype(np.float)) )/TimeSpent
 		meany=	meany+(data[i-1,5].astype(np.float))*(data[i,0].astype(np.float)-data[i-1,0].astype(np.float)) )/TimeSpent
-               
+		
+               	if(data[i-1,2]!=data[i,2]):
+			numberOfClicks=numberOfClicks+1
 	#For now only 4 features are used, more to be added.
  
 	avgMoves=(rows/TimeSpent)  
 	avgTime= (TimeSpent/rows)
-	features=np.array([avgTime,avgSpeed,TimeSpent,avgMoves,meanx,meany])
+	features=np.array([avgTime,avgSpeed,TimeSpent,avgMoves,meanx,meany,numberOfClicks])
 	featureVector=np.column_stack((featureVector,features))
