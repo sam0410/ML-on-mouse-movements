ce2a9cedb735a401e5861fb5f625b217a3a0dda9
Samikshya Chand <samikshya.chand.ece15@itbhu.ac.in>
2017-10-01 15:44:04 +0530

    edit description and correct avgx avgy

--- featureExtraction.py
+++ featureExtraction.py
@@ -2,7 +2,9 @@
 feature 1 (avgTime):calculates average time between moves
 feature 2 (avgSpeed):calculates a value similar to average speed of user moves
 feature 3 (TimeSpent):calculates time duration user spends online
-feature 4 (avgMoves):calculates avg number of by mouse movements per second  
+feature 4 (avgMoves):calculates avg number of by mouse movements per second
+feature 5 (avgx):avg x coordinate of mouse pointer when user changes coordinate
+
 '''
 FileList=["session_0041905381","session_1060325796","session_3320405034",  "session_3826583375"  ,"session_6668463071","session_8961330453","session_9017095287"]
 
@@ -20,18 +22,18 @@ for fileName in FileList:
 	avgSpeed=0
 	meanx=0
 	meany=0
+        TimeSpent=data[rows-1,0].astype(np.float)-data[0,0].astype(np.float)
 
 	for i in range (1,rows):
 		timeT=data[i,0].astype(np.float)-data[i-1,0].astype(np.float)
 		avgSpeed=avgSpeed+((np.linalg.norm([data[i,4].astype(np.float) - data[i-1,4].astype(np.float), data[i,5].astype(np.float) - data[i-1,5].astype(np.float)]))/(timeT*rows+1))
 		#to avoid NaN, 1 is added in the denominator
 		
-		meanx=	meanx+(data[i,4].astype(np.float))/rows ;
-		meany=	meany+(data[i,5].astype(np.float))/rows ;
+		meanx=	meanx+(data[i-1,4].astype(np.float))*(data[i,0].astype(np.float)-data[i-1,0].astype(np.float)) )/TimeSpent
+		meany=	meany+(data[i-1,5].astype(np.float))*(data[i,0].astype(np.float)-data[i-1,0].astype(np.float)) )/TimeSpent
                
 	#For now only 4 features are used, more to be added.
  
-	TimeSpent=data[rows-1,0].astype(np.float)-data[0,0].astype(np.float)
 	avgMoves=(rows/TimeSpent)  
 	avgTime= (TimeSpent/rows)
 	features=np.array([avgTime,avgSpeed,TimeSpent,avgMoves,meanx,meany])
