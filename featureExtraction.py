a45fd3edfb7b976f4bd91376fdae3774a50c88d9
Samikshya Chand <samikshya.chand.ece15@itbhu.ac.in>
2017-10-01 15:34:28 +0530

    Add 2 more features meanx and meany

--- featureExtraction.py
+++ featureExtraction.py
@@ -9,7 +9,7 @@ FileList=["session_0041905381","session_1060325796","session_3320405034",  "sess
 #FileList=["session_0041905381"]
 import numpy as np
 
-featureVector=[None] * 4
+featureVector=[None] * 6
 
 
 for fileName in FileList:
@@ -18,17 +18,21 @@ for fileName in FileList:
 	data=data[1:]
 	rows=data.shape[0]
 	avgSpeed=0
-
+	meanx=0
+	meany=0
 
 	for i in range (1,rows):
 		timeT=data[i,0].astype(np.float)-data[i-1,0].astype(np.float)
 		avgSpeed=avgSpeed+((np.linalg.norm([data[i,4].astype(np.float) - data[i-1,4].astype(np.float), data[i,5].astype(np.float) - data[i-1,5].astype(np.float)]))/(timeT*rows+1))
 		#to avoid NaN, 1 is added in the denominator
+		
+		meanx=	meanx+(data[i,4].astype(np.float))/rows ;
+		meany=	meany+(data[i,5].astype(np.float))/rows ;
                
 	#For now only 4 features are used, more to be added.
  
 	TimeSpent=data[rows-1,0].astype(np.float)-data[0,0].astype(np.float)
 	avgMoves=(rows/TimeSpent)  
 	avgTime= (TimeSpent/rows)
-	features=np.array([avgTime,avgSpeed,TimeSpent,avgMoves])
+	features=np.array([avgTime,avgSpeed,TimeSpent,avgMoves,meanx,meany])
 	featureVector=np.column_stack((featureVector,features))
